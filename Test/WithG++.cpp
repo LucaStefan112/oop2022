bd95167ee664fe6b49ebfe955f540a931741780c
lucacodefy <luca.tamas@codefysoftware.com>
2022-02-25 09:17:04 +0200

    source and executable compiled with g++ from comand line

---
+++ Test/WithG++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    //g++ source_file.cpp -o executable_file.exe
+    std::cout << "Executable with g++\n";
+    
+    system("pause");
+    
+    return 0;
+}
